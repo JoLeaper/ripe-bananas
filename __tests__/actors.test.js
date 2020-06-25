a8f41548999eaa366461b1e6a4fff55f65a8e5b2
JoLeaper <joleap123@gmail.com>
2020-06-25 14:14:39 -0700

    created test, actor data successfully seeded

---
+++ __tests__/actors.test.js
@@ -0,0 +1,24 @@
+const Actor = require('../lib/models/Actor');
+const app = require('../lib/app');
+const { prepare } = require('../db/data-helpers');
+// two get routes for actors (get all actors, get actor by id) and post route to update actors
+
+describe('ripe-bananas routes', () => {
+  it.only('GETs all the actors', async() => {
+    const actors = prepare(await Actor.find());
+
+    return app
+      .get('/api/v1/actors')
+      .then(res => {
+        expect(res.body).toEqual(actors);
+      });
+  });
+
+  it('GETs a single actor by id', () => {
+
+  });
+
+  it('UPDATEs a specific actors information', () => {
+
+  });
+});
--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,16 @@
-require('dotenv').config();
-require('./lib/utils/connect')();
+const chance = require('chance').Chance();
+const Actor = require('../lib/models/Actor');
+// const Film = require('../lib/models/Film');
+// const Review = require('../lib/models/Review');
+// const Reviewer = require('../lib/models/Reviewer');
+// const Studio = require('../lib/models/Studio');
 
-const mongoose = require('mongoose');
-const seed = require('./db/seed');
-
-seed()
-  .then(() => console.log('All Done!'))
-  .catch(err => console.error(`Error: ${err}`))
-  .finally(() => mongoose.connection.close());
+module.exports = async({ actors = 20 } = {}) => {
+  const actorArray = await Promise.all([...Array(actors)].map(async() => {
+    return Actor.create({
+      name: chance.name(),  
+      dob: chance.date(),
+      pob: chance.city() + ', ' + chance.state()
+    });
+  }));
+};
