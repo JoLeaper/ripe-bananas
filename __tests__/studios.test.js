4b1cf9786480dce79c77605ebaa6368047ce880f
JoLeaper <joleap123@gmail.com>
2020-06-25 15:31:53 -0700

    studio data seeded successfully

---
+++ __tests__/studios.test.js
@@ -0,0 +1,47 @@
+const Studio = require('../lib/models/Actor');
+const { prepare, agent } = require('../db/data-helpers');
+// two get routes for actors (get all actors, get actor by id) and post route to update actors
+
+describe('ripe-bananas routes', () => {
+  it('GETs all the actors', async() => {
+    const actors = prepare(await Actor.find());
+
+    return agent
+      .get('/api/v1/actors')
+      .then(res => {
+        expect(res.body).toEqual(actors);
+      });
+  });
+
+  it('GETs a single actor by id', async() => {
+    const actor = prepare(await Actor.findOne());
+
+    return agent
+      .get(`/api/v1/actors/${actor._id}`)
+      .then(res => {
+        expect(res.body).toEqual(actor);
+      });
+
+  });
+
+  it.only('POSTs a new actor and information', async() => {
+
+    return agent
+      .post('/api/v1/actors/')
+      .send({
+        name: 'john Jones',
+        dob: Date.now(),
+        pob: 'seattle WA'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          name: 'john Jones',
+          dob: expect.any(String),
+          pob: 'seattle WA',
+          __v:0
+
+        });
+      });
+  });
+});
--- db/seed.js
+++ db/seed.js
@@ -3,14 +3,25 @@ const Actor = require('../lib/models/Actor');
 // const Film = require('../lib/models/Film');
 // const Review = require('../lib/models/Review');
 // const Reviewer = require('../lib/models/Reviewer');
-// const Studio = require('../lib/models/Studio');
+const Studio = require('../lib/models/Studio');
 
-module.exports = async({ actors = 20 } = {}) => {
+module.exports = async({ actors = 20, studios = 10 } = {}) => {
   const actorArray = await Promise.all([...Array(actors)].map(async() => {
     return Actor.create({
       name: chance.name(),  
       dob: chance.date(),
       pob: chance.city() + ', ' + chance.state()
+    });
+  }));
+
+  const studioArray = await Promise.all([...Array(studios)].map(async() => {
+    return Studio.create({
+      name: chance.company(),  
+      address: {
+        city: chance.city(),
+        state: chance.state(),
+        country: chance.country()
+      }
     });
   }));
 };
