cb9bf87f0ed0053c57ecdd7be55a7f89b24d2b2e
langstonBS <langston.beckwithstanley@gmail.com>
2020-06-25 17:22:13 -0700

    revew post test

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -1,9 +1,11 @@
 const Review = require('../lib/models/Review');
+const Reviewer = require('../lib/models/Reviewer');
+const Film = require('../lib/models/Film');
 const { prepare, agent } = require('../db/data-helpers');
 
 
 describe('ripe-bananas routes', () => {
-  it.only('GETs all the reviews', async() => {
+  it('GETs all the reviews', async() => {
     const reviews = prepare(await Review.find());
 
     return agent
@@ -11,5 +13,30 @@ describe('ripe-bananas routes', () => {
       .then(res => {
         expect(res.body).toEqual(reviews);
       });
+  });
+  it('POSTs the review', async() => {
+    const reviewer = await Reviewer.findOne();
+    const film = await Film.findOne();
+
+    return agent
+      .post('/api/v1/reviews/')
+      .send({
+        rating: '2',
+        review: 'was okay but also shit',
+        reviewer: reviewer.id,
+        film: film.id
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          rating: '2',
+          review: 'was okay but also shit',
+          reviewer: reviewer.id,
+          film: film.id,
+          __v:0
+
+        });
+      });
+    
   });
 });
